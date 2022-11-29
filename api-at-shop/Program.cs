d493e2d5fa3a365222543daa870e37caea745205
Antun Delinger <antun994@gmail.com>
2022-11-29 18:27:29 +0100

    add database

--- api-at-shop/Program.cs
+++ api-at-shop/Program.cs
@@ -1,6 +1,8 @@
-﻿using api_at_shop.Services;
+﻿using api_at_shop.Repository;
+using api_at_shop.Services;
 using api_at_shop.Services.printify;
 using api_at_shop.Services.ProductServices;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,10 +12,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ShopConnection"));
+});
+
 builder.Services.AddScoped<IProductApiService, PrintifyService>();
-builder.Services.AddSingleton<ICurrencyService, HnbWebApiService>();
+builder.Services.AddScoped<ICurrencyService, HnbWebApiService>();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,8 +30,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-var url = builder.Configuration.GetConnectionString("PrintifyApiUrl");
 
 app.UseHttpsRedirection();
 
---
+++ api-at-shop/Repository/DataContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics.Metrics;
+using System.Net.Sockets;
+using api_at_shop.Model;
+
+namespace api_at_shop.Repository
+    {
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+
+        }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Currency>(b =>
+            {
+                b.HasKey(e => new { e.CurrencyID });
+
+            });
+
+        }
+        public DbSet<Currency> Currencies => Set<Currency>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json")
+                   .Build();
+                var connectionString = configuration.GetConnectionString("ShopConnection");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+    }
+
+}
