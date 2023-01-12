334e27ceeaff39cdc5a596a96c81ac091fc0cdd1
Antun Delinger <antun994@gmail.com>
2023-01-12 15:23:45 +0100

    update cors

--- api-at-shop/Program.cs
+++ api-at-shop/Program.cs
@@ -31,7 +31,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AtPolicy",     
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000", "at-classics.com")
+            policy.WithOrigins("http://localhost:3000", "at-classics.com", "https://at-shop.vercel.app")
                                 .AllowAnyHeader()
                                 .AllowAnyMethod();
         });
