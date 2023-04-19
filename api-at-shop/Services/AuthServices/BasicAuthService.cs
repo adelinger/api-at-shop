dafacdc706bd791d1b93fc5fd7430b49a9f51075
Antun Delinger <antun994@gmail.com>
2023-04-19 12:50:25 +0200

    move password to appsettings

--- api-at-shop/Services/AuthServices/BasicAuthService.cs
+++ api-at-shop/Services/AuthServices/BasicAuthService.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 using api_at_shop.Model.Auth;
 using api_at_shop.Services.AuthServices.Common;
+using Microsoft.Extensions.Configuration;
 
 namespace api_at_shop.Services.AuthServices
 {
     public class BasicAuthService : IBasicAuthService
     {
+        private readonly IConfiguration Configuration;
+        public BasicAuthService(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public async Task<BasicAuthUser> AuthUser(string username, string password)
         {
             try
             {
+                var api_pw = Configuration.GetSection("AppSettings").GetSection("InvoicesApiPassword").Value;
+
                 var authenticatedUser = new List<BasicAuthUser>
             {
-                new BasicAuthUser{Username = "admin", Password= "admin", ID=1},
-                new BasicAuthUser{Username = "antun", Password = "antun", ID=2},
+                new BasicAuthUser{Username = "autotoni", Password = api_pw, ID=3},
             };
 
                 var user = authenticatedUser.Where(user => user.Username == username && user.Password == password).SingleOrDefault();
